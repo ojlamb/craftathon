bd554dcffa81df4ded13cca730eba4ef61fe14af
katya-yani <katyayani.vyas@gmail.com>
2015-10-29 11:05:04 +0000

    stack setup

--- js/pharmFinderController.js
+++ js/pharmFinderController.js
@@ -1,5 +1,21 @@
 pharmFinder.controller('PharmFinderController', [function(){
 
+var self = this;
+
+self.searchResult = {
+  "items": [
+   {
+      "name": "boots",
+      "postcode": "SW1Y5DG",
+      "telephone": "123545398"
+    },
+    {
+      "name": "superdrug",
+      "postcode": "SW1Y5DG",
+      "telephone": "123545398"
+    }
+  ]
+};
 
 
 }]);
--- test/pharmFinderController.spec.js
+++ test/pharmFinderController.spec.js
@@ -7,8 +7,30 @@ describe('PharmFinderController', function() {
     ctrl = $controller('PharmFinderController');
   }));
 
-  it('initialises with an empty search result and term', function() {
+  xit('initialises with an empty search result and term', function() {
     expect(ctrl.searchResult).toBeUndefined();
     expect(ctrl.searchTerm).toBeUndefined();
   });
+
+  describe('when searching for a pharmacy', function() {
+
+  var items = [
+    {
+      "name": "boots",
+      "postcode": "SW1Y5DG",
+      "telephone": "123545398"
+    },
+    {
+      "name": "superdrug",
+      "postcode": "SW1Y5DG",
+      "telephone": "123545398"
+    }
+  ];
+
+  it('displays search results', function() {
+    expect(ctrl.searchResult.items).toEqual(items);
+  });
+});
+
+
 });
