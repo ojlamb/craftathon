4cbef71c549e2bbaa46b5a56e5c181f5a2f5410c
Owen Lamb <owen.lamb@gmail.com>
2015-11-03 12:01:19 +0000

    pans to marker when address clicked

--- js/map.js
+++ js/map.js
@@ -4,18 +4,23 @@ $(document).ready(function() {
         .setView([51.5072, -0.1], 13);
 
 
+  var marker = L.marker([0,0], {
+      icon: L.mapbox.marker.icon({
+          'marker-color': 'ff8888'
+      }),
+  });
   mapAddress = function() {
     $('.addressLink').click(function() {
       var address = $(this).text();
       $.getJSON("https://api.mapbox.com/geocoding/v5/mapbox.places/" + address + ".json?access_token=pk.eyJ1Ijoib3dlbmxhbWIiLCJhIjoiY2lleWljcnF4MDBiOXQ0bHR0anRvamtucSJ9.t3YnHHqvQZ8Y0MTCNy0NNw", function(result){
-
         var result = result.features[0].center;
-        console.log(result)
-        var marker = L.marker([result[1], result[0]], {
+        map.setView([result[1], result[0]], 15);
+        map.removeLayer(marker);
+        marker = L.marker([result[1], result[0]], {
             icon: L.mapbox.marker.icon({
                 'marker-color': 'ff8888'
             }),
-            draggable: true
+            draggable: false
         }).addTo(map);
     })
   });
