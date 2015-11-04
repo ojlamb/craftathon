40836a128e3810e8fd33a9a0c297a1f1867d9472
Owen Lamb <owen.lamb@gmail.com>
2015-11-04 15:46:56 +0000

    http connection

--- public/js/pharmFinderController.js
+++ public/js/pharmFinderController.js
@@ -2,7 +2,7 @@ pharmFinder.controller('PharmFinderController', ['$http', 'pharmSearchFactory','
 
   var self = this;
 
-  var searchResource = $http.get('www.serket.uk/pharmacies/pharmacylist-format').then(function(response) {
+  var searchResource = $http.get('https://www.serket.uk/pharmacies/pharmacylist-format').then(function(response) {
     self.searchResult = response.data.data.map(function(pharmacy) {
       var pharmacyAddress = [pharmacy[2], pharmacy[5], pharmacy[6], pharmacy[7]].join(', ');
       return { name: pharmacy[0], NACS: pharmacy[1], address: pharmacyAddress, telephone: pharmacy[8]}
